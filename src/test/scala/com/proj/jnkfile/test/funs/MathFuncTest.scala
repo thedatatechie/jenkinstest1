8b4d58f9892426578cd420b7e0d2b524138efc63
Vijaya Ramisetti <Vijaya.Ramisetti@royalmail.com>
2018-08-19 13:04:16 +0100

    tests added 1

--- src/test/scala/com/proj/jnkfile/test/funs/MathFuncTest.scala
+++ src/test/scala/com/proj/jnkfile/test/funs/MathFuncTest.scala
@@ -7,18 +7,18 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class MathFuncTest extends FunSuite with Matchers{
 
-//  test("testAdd") {
-//    val ob1 = new MathFunc()
-//    val result = ob1.add(3,5)
-//    val expected = 8
-//    result should be (expected)
-//  }
-//
-//  test("testMult") {
-//    val ob1 = new MathFunc()
-//    val result = ob1.mult(3,5)
-//    val expected = 15
-//    result should be (expected)
-//  }
+  test("testAdd") {
+    val ob1 = new MathFunc()
+    val result = ob1.add(3,5)
+    val expected = 8
+    result should be (expected)
+  }
+
+  test("testMult") {
+    val ob1 = new MathFunc()
+    val result = ob1.mult(3,5)
+    val expected = 15
+    result should be (expected)
+  }
 
 }
